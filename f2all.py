557d7f2a60ee9505a29776f4c526176461208bce
Miles <davis.g.miles@gmail.com>
2017-01-31 18:42:19 +1030

    starting command line arguments

--- f2all.py
+++ f2all.py
@@ -1,3 +1,10 @@
 #!/bin/python
 
-print("Hello world!")
+import getopt
+import sys
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "r")
+except getopt.GetoptError:
+	print("Usage: " + sys.argv[0])
+	sys.exit(1)
