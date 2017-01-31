12407b6eed75bae5e5d1bffa8ff56c91ac88d484
Miles <davis.g.miles@gmail.com>
2017-01-31 18:58:41 +1030

    added base name and starting number options

--- f2all.py
+++ f2all.py
@@ -10,23 +10,35 @@ rename = False
 # directory of files to rename - cwd by default
 filedir = os.getcwd()
 
+# base file name
+base = ""
+
+# file number
+num = 1
+
 # get cmd opts/args
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "rd:")
+	opts, args = getopt.getopt(sys.argv[1:], "rd:b:s:")
 except getopt.GetoptError:
 	print("Usage: " + sys.argv[0] +
-		" [-r] [-d directory]")
+		" [-r] [-d directory] [-b base-name] [-s start-num]")
 	sys.exit(1)
 
 # parse opts/args
 for opt, arg in opts:
-
 	# rename flag
 	if opt in "-r":
 		rename = True
 		print("raname = True")
-
-	# directory flag
+	# directory argument
 	elif opt in "-d":
 		filedir = arg
 		print("filedir = " + filedir)
+	# base name argument
+	elif opt in "-b":
+		base = arg
+		print("base = " + base)
+	# starting number argument
+	elif opt in "-s":
+		num = int(arg)
+		print("num = " + str(num))
