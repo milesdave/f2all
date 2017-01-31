c09f1b34d49b11820ba22735e51c1cf7c5291f00
Miles <davis.g.miles@gmail.com>
2017-01-31 19:58:49 +1030

    added glob support

--- f2all.py
+++ f2all.py
@@ -1,6 +1,7 @@
 #!/bin/python
 
 import getopt
+import glob
 import os
 import sys
 
@@ -19,17 +20,21 @@ num = 1
 # number of zeroes to pad file number with
 pad = 0
 
+# glob pattern
+globstr = ""
+
 # get cmd opts/args
 try:
-	opts, args = getopt.getopt(sys.argv[1:], "rd:b:s:z:")
+	opts, args = getopt.getopt(sys.argv[1:], "rd:b:s:z:g:")
 except getopt.GetoptError:
-	print("Usage: " + sys.argv[0] +
-		" [-r] [-d directory] [-b base-name] [-s start-num] [-z pad-num]\n\n"
+	print("Usage: f2all [-r] [-d directory] [-b base-name] [-s start-num]\n"
+		"             [-z pad-num] [-g pattern]\n\n"
 		"  -r                    perform the renaming\n"
 		"  -d directory          directory of files to rename\n"
 		"  -b base-name          base name for all files to use\n"
 		"  -s start-num          starting file number\n"
-		"  -z pad-num            number of zeroes to pad file number with")
+		"  -z pad-num            number of zeroes to pad file number with\n"
+		"  -g pattern            glob pattern for file matching")
 	sys.exit(1)
 
 for opt, arg in opts:
@@ -50,12 +55,15 @@ for opt, arg in opts:
 	# zero padding argument
 	elif opt in "-z":
 		pad = int(arg)
+	# glob argument
+	elif opt in "-g":
+		globstr = arg
 
-filelist = os.listdir(filedir)
+filelist = os.listdir(filedir) if not globstr else glob.glob(filedir + globstr)
 filelist.sort()
 
 for filename in filelist:
-	#build new file name
+	# build new file name
 	ext = os.path.splitext(filename)[1]
 	newname = base + str(num).zfill(pad) + ext
 
@@ -63,6 +71,6 @@ for filename in filelist:
 
 	# actually rename if -r set
 	if rename:
-		os.rename(filedir + filename, filedir + newname)
+		os.rename(filedir + filename if not globstr else filename, filedir + newname)
 
 	num += 1
